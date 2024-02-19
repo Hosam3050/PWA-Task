a2ffd89b14422aa635be4f9a2919b353238627e8
Hossam Hassan <138392814+Hosam3050@users.noreply.github.com>
2024-02-20 00:10:26 +0200

    Add files via upload

---
+++ register/sw.js
@@ -0,0 +1,41 @@
+this.addEventListener("install", (event) => {
+    console.log("service worker installed");
+    this.skipWaiting()
+    event.waitUntil(
+      caches
+        .open("simpleApp-1")
+        .then((cache) => {
+          return cache.addAll([
+            "register\register.html",
+            "register\register.css",
+            "test\login.html",
+            "test\login.css"
+          ]);
+        })
+        .catch((err) => console.log(err))
+    );
+  });
+
+  
+  this.addEventListener("activate", () => {
+    console.log("Inside activate phase");
+  });
+  
+
+  this.addEventListener("fetch", (event) => {
+    console.log(event.request.url);
+    event.respondWith(
+      caches
+        .match(event.request)
+        .then((file) => {
+          if (file) {
+            console.log("founded in cache");
+            return file;
+          }
+          console.log(event.request.url);
+          return fetch(event.request.url);
+        })
+        .catch((err) => console.log(err))
+    );
+  });
+  
